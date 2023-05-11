45fa2dafcdb9fb7d7351c15aee8c8ce81c65f72c
helenserb <serbinenkoifbb@gmail.com>
2023-05-11 06:14:28 -0400

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -2,7 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import { register, logIn, logOut, fetchCurrentUser } from './auth-operations';
+import {
+  register, 
+  logOut,
+  logIn, fetchCurrentUser
+} from './auth-operations';
 
 const authInitialState = {
   user: {
@@ -24,82 +28,82 @@ const persistConfig = {
 const authSlice = createSlice({
   name: 'auth',
   initialState: authInitialState,
-  // extraReducers:
-  //   builder =>
-  //   builder
-  //     .addCase(register.pending, () => {})
-  //     .addCase(register.rejected, () => {})
-  //     .addCase(register.fulfilled, (state, { payload }) => {
-  //       state.user.name = payload.user.name;
-  //       state.user.email = payload.user.email;
-  //       state.token = payload.token;
-  //       state.isLoggedIn = true;
-  //     })
-  //     .addCase(logIn.pending, () => {})
-  //     .addCase(logIn.rejected, () => {})
-  //     .addCase(logIn.fulfilled, (state, { payload }) => {
-  //       state.user.name = payload.user.name;
-  //       state.user.email = payload.user.email;
-  //       state.token = payload.token;
-  //       state.isLoggedIn = true;
-  //     })
-  //     .addCase(logOut.pending, (state, action) => state)
-  //     .addCase(logOut.rejected, (state, action) => state)
-  //     .addCase(logOut.fulfilled, () => authInitialState)
+  extraReducers:
+    builder =>
+    builder
+      .addCase(register.pending, () => {})
+      .addCase(register.rejected, () => {})
+      .addCase(register.fulfilled, (state, { payload }) => {
+        state.user.name = payload.user.name;
+        state.user.email = payload.user.email;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.pending, () => {})
+      .addCase(logIn.rejected, () => {})
+      .addCase(logIn.fulfilled, (state, { payload }) => {
+        state.user.name = payload.user.name;
+        state.user.email = payload.user.email;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.pending, (state, action) => state)
+      .addCase(logOut.rejected, (state, action) => state)
+      .addCase(logOut.fulfilled, () => authInitialState)
 
-  //     .addCase(
-  //       fetchCurrentUser.pending,
-  //       (state) => (state.isFetchingCurrentUser = true)
-  //     )
-  //     .addCase(fetchCurrentUser.fulfilled, (state, { payload }) => {
-  //       state.user.name = payload.name;
-  //       state.user.email = payload.email;
-  //       state.isLoggedIn = true;
-  //       state.isFetchingCurrentUser = false;
-  //     })
-  //     .addCase(
-  //       fetchCurrentUser.rejected,
-  //       (state) => { state.isFetchingCurrentUser = false }
-  //     )
+      // .addCase(
+      //   fetchCurrentUser.pending,
+      //   (state) => (state.isFetchingCurrentUser = true)
+      // )
+      .addCase(fetchCurrentUser.fulfilled, (state, { payload }) => {
+        state.user.name = payload.name;
+        state.user.email = payload.email;
+        state.isLoggedIn = true;
+        state.isFetchingCurrentUser = false;
+      })
+      .addCase(
+        fetchCurrentUser.rejected,
+        (state) => { state.isFetchingCurrentUser = false }
+      )
       
-  extraReducers: {
-    [register.pending]() {},
-    [register.fulfilled](state, { payload }) {
-      state.user.name = payload.user.name;
-      state.user.email = payload.user.email;
-      state.token = payload.token;
-      state.isLoggedIn = true;
-    },
-    [register.rejected]() {},
+  // extraReducers: {
+  //   [register.pending]() {},
+  //   [register.fulfilled](state, { payload }) {
+  //     state.user.name = payload.user.name;
+  //     state.user.email = payload.user.email;
+  //     state.token = payload.token;
+  //     state.isLoggedIn = true;
+  //   },
+  //   [register.rejected]() {},
 
-    [logIn.pending]() {},
-    [logIn.fulfilled](state, { payload }) {
-      state.user.name = payload.user.name;
-      state.user.email = payload.user.email;
-      state.token = payload.token;
-      state.isLoggedIn = true;
-    },
-    [logIn.rejected]() {},
+  //   [logIn.pending]() {},
+  //   [logIn.fulfilled](state, { payload }) {
+  //     state.user.name = payload.user.name;
+  //     state.user.email = payload.user.email;
+  //     state.token = payload.token;
+  //     state.isLoggedIn = true;
+  //   },
+  //   [logIn.rejected]() {},
 
-    [logOut.pending]() {},
-    [logOut.fulfilled]() {
-      return authInitialState;
-    },
-    [logOut.rejected]() {},
+  //   [logOut.pending]() {},
+  //   [logOut.fulfilled]() {
+  //     return authInitialState;
+  //   },
+  //   [logOut.rejected]() {},
 
-    [fetchCurrentUser.pending](state) {
-      state.isFetchingCurrentUser = true;
-    },
-    [fetchCurrentUser.fulfilled](state, { payload }) {
-      state.user.name = payload.name;
-      state.user.email = payload.email;
-      state.isLoggedIn = true;
-      state.isFetchingCurrentUser = false;
-    },
-    [fetchCurrentUser.rejected](state) {
-      state.isFetchingCurrentUser = false;
-    },
-  },
+  //   [fetchCurrentUser.pending](state) {
+  //     state.isFetchingCurrentUser = true;
+  //   },
+  //   [fetchCurrentUser.fulfilled](state, { payload }) {
+  //     state.user.name = payload.name;
+  //     state.user.email = payload.email;
+  //     state.isLoggedIn = true;
+  //     state.isFetchingCurrentUser = false;
+  //   },
+  //   [fetchCurrentUser.rejected](state) {
+  //     state.isFetchingCurrentUser = false;
+  //   },
+  // },
 });
 
 
